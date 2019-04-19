00bb1acb51198af3ff5097b198b77a98a4ec4137
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-04-19 18:50:58 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -5,5 +5,5 @@ CREATE TABLE bears (
   gender TEXT,
   color TEXT,
   temperament TEXT,
-  alive INTEGER, /*Boolean values are stored as integers 0 (false) and 1 (true).*/
+  alive INTEGER /*Boolean values are stored as integers 0 (false) and 1 (true).*/
 );
