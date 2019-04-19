7d5f782638dd39a2d3f0320793a029170b549ec0
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-04-19 18:55:34 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,15 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", "", "", "", "", "")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", "21", "", "", "", "")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", "4", "", "", "", "")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", "45", "", "", "", "")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", "54", "", "", "", "")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", "132", "", "", "", "")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", "54", "", "", "", "")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, "74", "", "", "", "")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "", "", "", "")
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 21, "", "", "", "")
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, "", "", "", "")
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 45, "", "", "", "")
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 54, "", "", "", "")
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 132, "", "", "", "")
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 54, "", "", "", "")
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 74, "", "", "", "")
