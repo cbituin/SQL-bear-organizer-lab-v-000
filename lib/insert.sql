5a520353c1f1936d50961ba9b8aea2ad22979023
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-04-19 18:56:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,12 +4,12 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy"
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, "", "", "", "")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 45, "", "", "", "")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 45, "", "", "", )
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 54, "", "", "", "")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 54, "", "", "", 0)
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 132, "", "", "", "")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 132, "", "", "", 1)
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 54, "", "", "", "")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 54, "", "", "", 1)
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 74, "", "", "", "")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 74, "", "", "", 1)
